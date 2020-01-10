a941a4aaf9a1ff3a278cd9c42249c5ffe570ec23
Nambyungwook <skaquddnr21@gmail.com>
2020-01-10 15:48:01 +0900

    first commit

---
+++ src/main/java/com/example/springwebservice/web/WebRestController.java
@@ -0,0 +1,26 @@
+package com.example.springwebservice.web;
+
+import com.example.springwebservice.dto.posts.PostsSaveRequestDto;
+import com.example.springwebservice.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsService postsService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+        return postsService.save(dto);
+    }
+}
